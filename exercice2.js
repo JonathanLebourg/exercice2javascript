ed5285daa775f93127d13cc75c14713e7d5d5cb2
JonathanLebourg <jonjonlebourg@gmail.com>
2018-10-09 14:14:14 +0200

    mis en page

--- exercice2.js
+++ exercice2.js
@@ -2,4 +2,5 @@ var lastName= "Norris";
 var firstName= "Chuck";
 var city= "Dallas";
 
-alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city);
+// affichage dans une boite
+alert("Nom: "+lastName+"\nPrénom: "+firstName+"\nVille: "+city); 
